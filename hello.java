5980f40191aeb935d6edbb88243831846ec3dfc7
kenyonchristie <98219181+kenyonchristie@users.noreply.github.com>
2023-03-22 11:35:51 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,10 +5,9 @@ public class hello {
     public static void main(String [] args) {
 
        // initialize variables
-       String firstName = "Alex";
+       String firstName = "Christie";
 	
        // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
-
+	System.out.println("Hello, I am " + firstName + ".");
     }
 }
-
